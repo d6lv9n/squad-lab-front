6ea3ffd159c99a711ce797d7b66e4a8290149044
Delvin <work.d6lv9n.github@gmail.com>
2025-06-30 15:59:08 +0700

    Add @ alias to simplify root directory imports

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
@@ -6,4 +7,9 @@ export default defineConfig({
   plugins: [
     vue()
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 });
