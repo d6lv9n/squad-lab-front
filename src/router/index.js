babdef808e4a8393c3cfed3ce8348c2975594916
Delvin <work.d6lv9n.github@gmail.com>
2025-06-30 16:34:44 +0700

    Import account routes and apply to routes array

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
+import account from './account.js';
 import { createRouter, createWebHistory } from 'vue-router';
 
 export default createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        account,
+    ]
 });
