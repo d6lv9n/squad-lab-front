0b57bf2e205f3985ba6f0c0a8decbeb2ddc1b522
Delvin <work.d6lv9n.github@gmail.com>
2025-07-01 15:29:29 +0700

    Set up Vuex module

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex/types/index.js';
+// Modules
+
+const store = createStore({
+    // devtools: true,
+    strict: false,
+    modules: {
+
+    }
+});
+
+export default store;
