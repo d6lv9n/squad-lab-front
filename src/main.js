4ba8b267a47cdba67ee604101de24e07f49edaf5
Delvin <work.d6lv9n.github@gmail.com>
2025-07-01 15:32:00 +0700

    Add axios configuration, move route navigation guard to router index file for better management

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import './style.css';
+// import store from './store/index.js';
 import router from './router/index.js';
 import { createApp } from 'vue';
-import Root from './app/Root.vue';
+import AppRoot from './app/Root.vue';
+import axios from 'axios';
 
-const App = createApp(Root);
+// Axios configuration
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+window.axios = axios;
 
+// Create Vue app
+const App = createApp(AppRoot);
+
+// Integrate plugins with Vue app
+// App.use(store);
 App.use(router);
 
 router.isReady()
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
+import store from '../store/index.js';
+import { createRouter, createWebHistory } from 'vue-router';
+// Routes
 import account from './routes/account.js';
 import index from './routes/index.js';
-import { createRouter, createWebHistory } from 'vue-router';
 
-export default createRouter({
+// Create Router
+const router = createRouter({
     history: createWebHistory(),
     routes: [
         account,
         index,
     ]
 });
+
+// Navigation guard
+router.beforeEach((to, from) => {
+    return true;
+});
+
+router.beforeResolve(to => {
+    return true;
+});
+
+router.afterEach((to, from, failure) => {
+
+});
+
+export default router;
