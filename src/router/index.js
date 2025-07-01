4add59b4a9f38ac9a9fc3165b24b8adb14306f1e
Delvin <work.d6lv9n.github@gmail.com>
2025-07-01 14:53:19 +0700

    Move account routes to routes directory for better future route management

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
-import account from './account.js';
+import account from './routes/account.js';
+import index from './routes/index.js';
 import { createRouter, createWebHistory } from 'vue-router';
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
         account,
+        index,
     ]
 });
